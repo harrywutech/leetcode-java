634f87cb3e95600a343894f8c05b3a2d1809e214
Harry Wu <harry.wu.tech@gamil.com>
2024-09-25 20:56:04 -0700

    Selection Sort Generic

--- src/SortingFundamental/SelectionSort.java
+++ src/SortingFundamental/SelectionSort.java
@@ -4,27 +4,25 @@ import java.sql.SQLOutput;
 
 public class SelectionSort {
     private SelectionSort() {}
-
-    public static void sort(int[] arr) {
+    public static <E extends Comparable<E>> void sort(E[] arr) {
         for (int i = 0; i < arr.length; i++) {
             //select arr[i...n) minimal index
             int minIndex = i;
             for (int j = i; j < arr.length; j++) {
-                if(arr[j] < arr[minIndex])
+                if(arr[j].compareTo(arr[minIndex]) < 0)
                     minIndex = j;
             }
             swap(arr, i, minIndex);
         }
     }
-
-    private static void swap(int[] arr, int i, int j) {
-        int t = arr[i];
+    private static <E> void swap(E[] arr, int i, int j) {
+        E t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
     }
 
     public static void main(String[] args) {
-        int[] arr={1, 4, 2, 3, 6, 5};
+        Integer[] arr={1, 4, 2, 3, 6, 5};
         SelectionSort.sort(arr);
 
         for(int e: arr)
