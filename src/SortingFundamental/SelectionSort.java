515a5f7790bc9756ef99393a5433f70aecd40559
Harry Wu <harry.wu.tech@gamil.com>
2024-10-06 20:31:19 -0700

    complexity analysis

--- src/SortingFundamental/SelectionSort.java
+++ src/SortingFundamental/SelectionSort.java
@@ -1,6 +1,8 @@
 package SortingFundamental;
 
 import LinearSearch.Student;
+import Tools.ArrayGenerator;
+import Tools.SortingHelper;
 
 import java.sql.SQLOutput;
 
@@ -24,20 +26,11 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
-        Integer[] arr={1, 4, 2, 3, 6, 5};
-        SelectionSort.sort(arr);
 
-        for(int e: arr)
-            System.out.println(e + " ");
-        System.out.println();
-
-        Student[] students = {new Student("Alice", 98),
-                new Student("Bobo", 100),
-                new Student("Charles", 66)};
-        SelectionSort.sort(students);
-
-        for(Student student: students)
-            System.out.println(student + " ");
-        System.out.println();
+        int[] dataSize = {10000, 100000};
+        for (int n : dataSize) {
+            Integer[] arr= ArrayGenerator.generateRamdomArray(n, n);
+            SortingHelper.sortTest("SelectionSort", arr);
+        }
     }
 }
--- src/Tools/ArrayGenerator.java
+++ src/Tools/ArrayGenerator.java
@@ -1,5 +1,7 @@
 package Tools;
 
+import java.util.Random;
+
 public class ArrayGenerator {
     private ArrayGenerator() {
     }
@@ -9,6 +11,18 @@ public class ArrayGenerator {
         Integer[] arr = new Integer[n];
         for (int i = 0; i < n; i++) {
             arr[i] = i;
+        }
+        return arr;
+    }
+
+    //generate a length of n ramdom array, range [0, bound)
+    public static Integer[] generateRamdomArray(int n, int bound) {
+
+        Integer[] arr = new Integer[n];
+
+        Random rnd = new Random();
+        for(int i = 0; i < n; i ++) {
+            arr[i] = rnd.nextInt(bound);
         }
         return arr;
     }
